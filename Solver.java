61614af22c158f96e3397f3d6fb4a7ddc213da9a
Benjamin Lim <blim038@ucr.edu>
2025-02-08 23:31:03 -0800

    implemented a user input method where the user can choose the 8puzzle that is being solved, along with error messages if user enters a non-number, number 9, or repeats an integer

--- Solver.java
+++ Solver.java
@@ -1,21 +1,15 @@
 package puzzle8;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Solver {
 	public static void main(String[] args) {
-		
-		// All the boards.
-		int [][] easy = {{1,3,4},{8,6,2},{7,0,5}}; // first test case on easy difficulty
-		int [][] medium = {{2,8,1},{0,4,3},{7,6,5}}; // second test case on medium difficulty
-		int [][] hard = {{5,6,7},{4,0,8},{3,2,1}}; // third test case on hard difficulty
+		// initialize user data storing method with scanner
+		int[][] userInput = new int[3][3];
+		BoardNode userNode = new BoardNode(userInput);
+		Scanner scanner = new Scanner(System.in); 
+		HashSet<Integer> trackIntegers = new HashSet<>();
 
-		// TODO - fix these test cases to take any inputs from the user
-
-		// Initial Nodes for 3 Levels
-		BoardNode easyNode = new BoardNode(easy);
-		BoardNode mediumNode = new BoardNode(medium);
-		BoardNode hardNode = new BoardNode(hard);
-		
 		// search waiting to be initialized
 		Search search = null;
 		
@@ -25,6 +19,33 @@ public class Solver {
 	while(con==true){  //the loop keeps going till User says no more to using the solver
 		System.out.println();
 		System.out.println("Welcome to 8 puzzle"); //Below are the options asking User for which search and what difficulty to pick
+		System.out.println("Enter tiles in order going from top left all the way to bottom right going across (with 0 being the empty tile)");
+		System.out.println();
+
+		for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                while (true) { // Keep asking until a valid input is received
+                    if (!scanner.hasNextInt()) { // Check if the input is an integer
+                        System.out.println("Invalid input! Please enter a valid integer.");
+                        scanner.next(); // Discard the invalid input
+                        continue; // Ask for input again
+                    }
+
+                    int num = scanner.nextInt();
+
+                    if (num == 9) { // Check if the number is 9
+                        System.out.println("Error! The number 9 is not allowed. Please enter a different number.");
+                    } else if (trackIntegers.contains(num)) { // Check for duplicates
+                        System.out.println("Number already entered! Please enter a unique number.");
+                    } else {
+                        trackIntegers.add(num); // Add valid number to HashSet
+                        userInput[i][j] = num; // Store in matrix
+                        break; // Exit the loop since input is valid
+                    }
+                }
+            }
+        }
+		
 		System.out.println("Please chose an Algorithm below:");
 		System.out.println();
 		System.out.println();
@@ -33,37 +54,12 @@ public class Solver {
 	
 		System.out.println("2. A*");
 		System.out.println();
-		Scanner scanner = new Scanner(System.in); // TODO - FIX "Resource leak: 'scanner' is never closed" warning
 		int input = scanner.nextInt(); // variable that saves user's first input of which algorithm to use
-		
-		System.out.println("Now chose a difficulty");
-		System.out.println();
-		System.out.println();
-		System.out.println("1. Easy");
-		
-		System.out.println("2. Medium");
-		
-		System.out.println("3. Hard");
-		System.out.println();
-		
-		int input2 = scanner.nextInt(); // variable that saves user's second input of difficulty
 		
 		switch(input) { //switch is used to determine what search and difficulty to use
 			
 		case 1:							
-			switch(input2){ // second switch is used to determine the difficulty and set the node's to their respective parameters
-				case 1: 
-					search = new UniformCost(easyNode);
-					break;
-				case 2: 
-					search = new UniformCost(mediumNode);
-					break;
-				case 3: 
-					search = new UniformCost(hardNode);
-					break;
-			}
-
-			break;
+			search = new UniformCost(userNode);
 			
 		case 2:  //final case for A* which provides options for both Heuristics
 			System.out.println();
@@ -72,40 +68,13 @@ public class Solver {
 			System.out.println("1. Misplaced Tiles");
 			System.out.println("2. Manhattan");
 			System.out.println();
-			int input3 = scanner.nextInt();
+			int input2 = scanner.nextInt();
 			
-			switch(input3){	// third switch that chooses which A* algorithm to use		
+			switch(input2){	// third switch that chooses which A* algorithm to use		
 				case 1: 
-					switch(input2) { // second switch that sets difficulty for Misplaced Tiles method
-					
-					case 1:
-						search = new Astar(easyNode,1);
-						break;
-					
-					case 2: 
-						search = new Astar(mediumNode,1);
-						break;
-					case 3: 
-						search = new Astar(hardNode,1);
-						break;
-				}
-				
-				break;
-
+					search = new Astar(userNode,1);
 				case 2:
-						switch(input2) { // second switch that sets difficulty for Manhattan search
-					
-					case 1:
-						search = new Astar(easyNode,2);
-						break;
-					
-					case 2: 
-						search = new Astar(mediumNode,2);
-						break;
-					case 3: 
-						search = new Astar(hardNode,2);
-						break;
-				}
+					search = new Astar(userNode,2);
 			}
 					
 		}
